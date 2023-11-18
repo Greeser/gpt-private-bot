58d324727ca2d97f0382484a8cd5f1020b9f7f75
Greeser <domnich.artem@gmail.com>
2023-11-18 13:05:06 +0200

    fix startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
-python3 -m venv .env
+python -m venv .env
 source .env/bin/activate
-python3 -m pip install --upgrade pip
-python3 -m pip install -r ./requirements.txt
-python3 -m src.main
+python -m pip install --upgrade pip
+python -m pip install -r ./requirements.txt
+python -m src.main
